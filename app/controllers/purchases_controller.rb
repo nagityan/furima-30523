58a03edb1054d7120b984d27a8079837e0ee349e
nagityan <61828492+nagityan@users.noreply.github.com>
2020-10-11 12:19:30 +0900

    フォームオブジェクト作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,32 @@
 class PurchasesController < ApplicationController
   def new
+    @item = Item.find(params[:item_id])
+    @user_purchase = UserPurchase.new
   end
+
   def create
+    @item = Item.find(params[:item_id])
+    @user_purchase = UserPurchase.new(params_data)
+    if @user_purchase.valid?
+      pay_item
+      @user_purchase.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def params_data
+    params.require(:user_purchase).permit(:postal_code,:prefecture_id,:city,:address,:building,:phone_number).merge(token:params[:token],item_id: params[:item_id],user_id:current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+      price = Item.find(item_id: item_id).price 
+        Payjp::Charge.create(
+          amount: price,
+          card: token,    
+          currency: 'jpy')
   end
 end
---
+++ app/models/user_purchase.rb
@@ -0,0 +1,24 @@
+class UserPurchase
+  include ActiveModel::Model
+
+
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city,:address,:building, :phone_number,:token,:purchase_id
+  
+  with_options presence: true do
+    validates :postal_code
+    validates :city
+    validates :address
+    validates :phone_number
+    validates :token
+  end
+
+  validates :prefecture_id,numericality: { other_than: 1 }
+
+  def save
+    purchase = Purchase.create(user_id: user_id,item_id: item_id)
+    Address.create(postal_code: postal_code,prefecture_id: prefecture_id,city: city,address: address,building: building,phone_number: phone_number,purchase_id: purchase.id)
+  end
+ 
+
+
+end
