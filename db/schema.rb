b683640901658fbaf5d1530ea17bf9bf362f31ca
nagityan <61828492+nagityan@users.noreply.github.com>
2020-10-11 12:19:40 +0900

    その他

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_093954) do
+ActiveRecord::Schema.define(version: 2020_10_10_131105) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
